bfe1df7f37010fa1682f604e3b0f17d3378c2ce5
Tkuhn9 <75342996+Tkuhn9@users.noreply.github.com>
2024-05-15 13:42:36 -0600

    Update FamilyStatsDataCleaning.sql

--- SQL_Project1/SQL_Code/FamilyStatsDataCleaning.sql
+++ SQL_Project1/SQL_Code/FamilyStatsDataCleaning.sql
@@ -1,17 +1,13 @@
---Make a Copy of Mariage Rate Table for Data Cleaning
-/*CREATE TABLE marriage_rate LIKE [dbo].['marriage-rate-per-1000-inhabita$']
-INSERT INTO marriage_rate 
-	SELECT Distinct Entity, Code, [Year], [Crude marriage rate (per 1,000 inhabitants)] 
-	FROM [dbo].['marriage-rate-per-1000-inhabita$']
-*/
-
+--Make a Copy of the Marriage Rate Table with no duplicates
 SELECT DISTINCT Entity, Code, [Year], [Crude marriage rate (per 1,000 inhabitants)] 
 	INTO marriage_rate
 	FROM [dbo].['marriage-rate-per-1000-inhabita$']
-
+	
+--Make a Copy of the Divorce Rate Table with no duplicates
 SELECT DISTINCT Entity, Code, [Year], [Crude divorce rate (per 1,000 inhabitants)] 
 	INTO divorce_rate
 	FROM [dbo].['divorces-per-1000-people$']
 
+--Remove the old tables with duplicates
 DROP TABLE [dbo].['marriage-rate-per-1000-inhabita$']
 DROP TABLE [dbo].['divorces-per-1000-people$']
