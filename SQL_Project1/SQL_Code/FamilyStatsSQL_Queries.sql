74b3f7a96d8a2734d974d4d66179bc2d788e40f7
Tkuhn9 <75342996+Tkuhn9@users.noreply.github.com>
2024-05-20 15:24:33 -0600

    Update FamilyStatsSQL_Queries.sql

--- SQL_Project1/SQL_Code/FamilyStatsSQL_Queries.sql
+++ SQL_Project1/SQL_Code/FamilyStatsSQL_Queries.sql
@@ -1,4 +1,5 @@
---PROJECT EXPLANATION
+--PROJECT EXPLANATION: This project was built to create a data source for my Tableau project while also displaying some of my querying abilities in SQl.
+--This project investigates trends in marriages and divorces, as rates (out of 1000 people), in total amounts, and as a ratio between marriages and divorces.
 --Initial Data Exploration
 	--Select entire marriage rate table
 Select * FROM [dbo].[marriage_rate]
