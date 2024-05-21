bdfd7687510309c2ba6d5f6eb4b302309378083f
Tkuhn9 <75342996+Tkuhn9@users.noreply.github.com>
2024-05-21 12:26:01 -0500

    Update Data Cleaning.sql

--- SQL_Project1/SQL_Code/Data Cleaning.sql
+++ SQL_Project1/SQL_Code/Data Cleaning.sql
@@ -1,13 +1,14 @@
---Make a Copy of the Marriage Rate Table with no duplicates
+--PURPOSE: To remove the duplicate data found in both the Marriage Rate and Divorce Rate tables
+--Step 1: Make a Copy of the Marriage Rate Table with no duplicates
 SELECT DISTINCT Entity, Code, [Year], [Crude marriage rate (per 1,000 inhabitants)] 
 	INTO marriage_rate
 	FROM [dbo].['marriage-rate-per-1000-inhabita$']
 	
---Make a Copy of the Divorce Rate Table with no duplicates
+--Step 2: Make a Copy of the Divorce Rate Table with no duplicates
 SELECT DISTINCT Entity, Code, [Year], [Crude divorce rate (per 1,000 inhabitants)] 
 	INTO divorce_rate
 	FROM [dbo].['divorces-per-1000-people$']
 
---Remove the old tables with duplicates
+--Step 3: Remove the old tables with duplicates
 DROP TABLE [dbo].['marriage-rate-per-1000-inhabita$']
 DROP TABLE [dbo].['divorces-per-1000-people$']
